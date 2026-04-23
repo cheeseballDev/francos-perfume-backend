262c20b0aa072afe624aead1310003b9ab9a4ef0
cheeseball <cheeseballbusinessemail@gmail.com>
2026-04-23 11:53:53 +0800

    [FIX] Fixed duplicate code lines

--- InventorySystemBackend/DTOs/InventoryDTOs/InventoryDisplayDTO.cs
+++ InventorySystemBackend/DTOs/InventoryDTOs/InventoryDisplayDTO.cs
@@ -14,13 +14,8 @@
         public string? product_image_url { get; set; }
         public DateTime product_date_created { get; set; }
 
-<<<<<<< HEAD
         public string? branch_name { get; set; }
         //public string? branch_display_id { get; set; }
-=======
-        //public string? branch_display_id { get; set; }
-        public string? branch_name { get; set; }
->>>>>>> 7df4d5774448c9cce27e8bb8ddea57448a0c9c3b
         public int product_qty { get; set; }
     }
 }
